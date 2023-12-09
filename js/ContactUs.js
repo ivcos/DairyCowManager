1e726c41495a6c093a8f72033f2c6abf1c1f792c
ivcos <135237197+ivcos@users.noreply.github.com>
2023-12-09 11:02:17 +0000

    add changes to ContactUs Form

--- js/ContactUs.js
+++ js/ContactUs.js
@@ -1,3 +1,25 @@
+//Function to submit Form Data
+function submitContactFormData(event) {
+  console.log("Submit Contact Form Data");
+  let messageContactForm = document.getElementById(
+    "message-contact-form-submitted"
+  );
+  let formContactFirstName = document.getElementById("form-first-name");
+  let formContactLastName = document.getElementById("form-last-name");
+  let formEmail = document.getElementById("form-email");
+  if (
+    formContactFirstName.value.trim() === "" ||
+    formContactLastName.value.trim() === "" ||
+    formEmail.value.trim() === ""
+  ) {
+    messageContactForm.textContent = "PLEASE ENTER ALL THE REQUIRED FIELDS";
+    messageContactForm.style.display = "block";
+  } else {
+    messageContactForm.textContent = "DATA ENTERED SUCCESSFULLY";
+    messageContactForm.style.display = "block";
+  }
+}
+
 function onDOMLoaded() {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".navbar");
@@ -6,10 +28,15 @@ function onDOMLoaded() {
     nav.classList.toggle("active");
     burger.classList.toggle("toggle");
   });
-  // document.querySelector("#Clear").addEventListener("click", function (event) {
-  //   clearForm(event);
-  //   console.log("Form Cleared");
-  // }); // <-- added closing parenthesis here//
+
+  document
+    .querySelector("#submit_contact_form")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // When this is added the default form is not submitted
+      console.log("Item 10: Submiting Contact Form Data");
+      submitContactFormData(event);
+      console.log("Item 10a: Submiting Contact Form Data");
+    });
 }
 
 // Attach the onDOMLoaded function to the DOMContentLoaded event
